f1b0f2b35a0d7e32812def25f69df411cf4d21d3
Mishenina Evgenia <eamishenina@gmail.com>
2026-04-07 16:59:15 +0300

    Initial Commit

---
+++ Chuck_Norris_Jokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chuck_Norris_Jokes
+//
+//  Created by g1nz4 on 07.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
